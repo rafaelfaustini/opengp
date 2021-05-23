71df99e0c93c31be5c5fcd461d15ec762e64dd58
Rafael Faustini <contato@rafaelfaustini.com.br>
2021-05-23 13:37:13 -0300

    Organization

--- project/OpenGeopolitical/Assets/scripts/Installers/TimeEventsInstaller.cs
+++ project/OpenGeopolitical/Assets/scripts/Installers/TimeEventsInstaller.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
 
-public class GameObjectsInstaller : MonoInstaller
+public class TimeEventsInstaller : MonoInstaller
 {
     public Text textoTempo;
     public Button pauseButton;
